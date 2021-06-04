b6b570825f869ee5a7c8621920182afd96328e83
clumsyCoder00 <nuthanael.watkins@gmail.com>
2021-06-04 17:22:26 -0400

    Add files via upload
    omitting python-snapcast dependency, communicating directly with snapcast API.

--- Snapcast/snapcastStreamSelect.py
+++ Snapcast/snapcastStreamSelect.py
@@ -1,37 +1,38 @@
-import asyncio
-# pip install snapcast
-import snapcast.control
-import time
+import time, json, requests; 
 
-# https://github.com/happyleavesaoc/python-snapcast
-loop = asyncio.get_event_loop()
-
-#activeStreamCount = 0
 activeStream = ''
+# https://github.com/badaix/snapcast/blob/master/doc/json_rpc_api/v2_0_0.md#groupsetstream
+# https://github.com/badaix/snapcast/blob/master/control/scripts.md
 
-# print all client names
-#print("\nClients")
-#for client in server.clients:
-#  print(client.friendly_name)
+url = 'http://127.0.0.1:1780/jsonrpc'
+headers = {'Content-Type': 'application/json'}
+values = {
+	"id":1,
+	"jsonrpc":"2.0",
+	"method": "Server.GetStatus"
+	}
 
-# print all stream names
-#print("\nStreams")
 while True:
-	server = loop.run_until_complete(snapcast.control.create_server(loop, 'localhost'))
 	activeStreamCount = 0
-	for stream in server.streams:
-		#print(stream.friendly_name + " " + stream.status + " " + stream.identifier)
-		if stream.status == 'playing':
-			#print(stream.friendly_name)
+	req = requests.post(url, data=json.dumps(values), headers=headers)
+	req_json = json.loads(req.text)
+
+	for thisStream in req_json['result']['server']['streams']:
+		if thisStream['status'] == 'playing':
 			activeStreamCount +=1
-			activeStream = stream.identifier
+			activeStream = thisStream['id']
 
 	if activeStreamCount == 1:
-		# print all groups
-		#print("\nGroups")
-		for group in server.groups:
-			#print(group.friendly_name + " " + group.stream + " " + group.identifier)
-			loop.run_until_complete(server.group_stream(group.identifier, activeStream))
-	time.sleep(1)
-
-# server.on_stream_update
+		for thisGroup in req_json['result']['server']['groups']:
+			if thisGroup['stream_id'] != activeStream:
+				set_stream_values = {
+					"id":4,
+					"jsonrpc":"2.0",
+					"method": "Group.SetStream",
+					"params": {"id":thisGroup['id'],"stream_id":activeStream}
+					}
+				req = requests.post(url, data=json.dumps(set_stream_values), headers=headers)
+				print("\nset group: " + thisGroup['id'] + " stream to: " + activeStream)
+		
+		print('.', end='', flush=True)
+		time.sleep(1)
